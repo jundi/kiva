d04cb64c4d50dfa696b576a065261b494ca8cb91
Heikki Mikkolainen <heikki.mikkolainen@gmail.com>
2021-07-12 21:10:44 +0300

    Error handling

--- kiva/app.py
+++ kiva/app.py
@@ -1,6 +1,7 @@
 """Start Flask application"""
 import uuid
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, abort
+import werkzeug
 from kiva.tournament import Tournament
 
 
@@ -16,15 +17,32 @@ def create_app():
             return render_template('create.html', url=url_for('_create'))
 
         teams = request.form['teams'].splitlines()
+
+        # Remove empty teams
+        teams = [team for team in teams if team]
+
+        if len(teams) < 3:
+            abort(400, "At least three teams is rquired.")
+
+        # Create new tournament
         identifier = uuid.uuid4().hex[:6]
         tournaments[identifier] = Tournament(teams)
 
+        # Forward to schedule
         return redirect(f'/schedule/{identifier}')
 
     @app.route('/schedule/<identifier>', methods=['GET'])
     def _schedule(identifier):
         return render_template('schedule.html',
                                schedule=tournaments[identifier].schedule)
+
+    @app.errorhandler(werkzeug.exceptions.HTTPException)
+    def _handle_exception(exception):
+        """Return nice error page."""
+        return render_template('error.html',
+                               code=exception.code,
+                               name=exception.name,
+                               description=exception.description)
 
     return app
 
